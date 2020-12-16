949915f267c71334ba70ceeb6af3953b32d9dee9
romulovieira777 <romulo.vieira777@gmail.com>
2020-12-16 19:15:02 -0300

    Conjuntos

---
+++ Seção 06 - Conjuntos/conjuntos.py
@@ -0,0 +1,16 @@
+# Criando um Conjunto
+print({1, 2, 3})
+print(type({1, 2, 3}))
+
+
+# Conjunto não aceita Valores Duplicados
+print({1, 2, 3, 3, 3, 3, 3})
+
+
+# Criando um Conjunto
+conjunto = {1, 2, 3}
+print(conjunto)
+
+
+# Contando os Elementos do Conjunto
+print(len(conjunto))
