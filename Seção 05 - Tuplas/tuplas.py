c73a28374151c580f8bb945a37a6ad7b83a1f16f
romulovieira777 <romulo.vieira777@gmail.com>
2020-12-16 19:04:41 -0300

    Tuplas

---
+++ Seção 05 - Tuplas/tuplas.py
@@ -0,0 +1,24 @@
+# Criando uma Tupla
+nomes = ('Ana', 'Bia', 'Gui', 'Ana')
+print(type(nomes))
+print(nomes)
+
+
+# Para saber se um Nome está na Tupla
+print('Bia' in nomes)
+
+
+# Buscando um Dado Específico na Tupla
+print(nomes[0])
+
+
+# Acessando mais de um Dado na Tupla
+print(nomes[1:3])
+print(nomes[1:-1])
+print(nomes[2:])
+print(nomes[:-2])
+
+
+# Verificando o Tamanho da Tupla
+print(len(nomes))
+
