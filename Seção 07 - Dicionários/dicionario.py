3366b13c6f07f4df6d3143fc55b6e81c1d1f05c2
romulovieira777 <romulo.vieira777@gmail.com>
2020-12-16 19:22:36 -0300

    Dicionários

---
+++ Seção 07 - Dicionários/dicionario.py
@@ -0,0 +1,18 @@
+# Criando um Dicionário
+aluno = {
+    'nome': 'Pedro Henrique',
+    'nota': 9.2,
+    'ativo': True
+}
+
+print(type(aluno))
+
+
+# Acessando um Valor Específico do Dicionário
+print(aluno['nome'])
+print(aluno['nota'])
+print(aluno['ativo'])
+
+
+# Contando Quantos pares Associados dentro do Dicionário
+print(len(aluno))
