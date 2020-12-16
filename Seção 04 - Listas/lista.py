bcfff384ad98f4665d827d9c05036d6a4475db1e
romulovieira777 <romulo.vieira777@gmail.com>
2020-12-16 18:47:47 -0300

    Listas

---
+++ Seção 04 - Listas/lista.py
@@ -0,0 +1,27 @@
+# Criando uma Lista
+numeros = [1, 2, 3]
+print(type(numeros))
+
+
+# Adicionando Dados na Lista Criada
+numeros.append(4)
+numeros.append(5)
+numeros.append(6)
+print(len(numeros))
+
+
+# Alterando um Número da Lista
+numeros[5] = 100
+
+
+# Inserindo Dados na Lista
+numeros.insert(6, 7)
+print(numeros)
+
+
+# Selecionando um Dado Específico da Lista
+print(numeros[6])
+
+
+# Pegando o Último Elemento da Lista
+print(numeros[-1])
