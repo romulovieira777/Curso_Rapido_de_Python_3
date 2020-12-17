5b7a591208eaec16c6eac40f3eac6b5df5c72b17
romulovieira777 <romulo.vieira777@gmail.com>
2020-12-17 18:57:06 -0300

    Operadores Aritméticos

---
+++ Seção 09 - Operadores Aritméticos/operadores_aritmeticos.py
@@ -0,0 +1,31 @@
+# Declarando Variáveis
+x = 10
+y = 3
+
+
+# Somando o Valor das Duas Variáveis
+print(x + y)
+
+
+# Subtraindo o Valor das Duas Variáveis
+print(x - y)
+
+
+# Multiplicando o Valor das Duas Variáveis
+print(x * y)
+
+
+# Dividindo o Valor das Duas Variáveis
+print(x / y)
+
+
+# Pegando o Resto da Divisão
+print(x % y)
+
+
+# Para saber se o Número é Par ou Ímpar
+par = 34
+impar = 33
+
+print(par % 2 == 0)
+print(impar % 2 == 1)
