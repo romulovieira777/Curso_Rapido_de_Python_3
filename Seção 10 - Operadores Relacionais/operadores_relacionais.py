dd98a7d6f39f26b9e94c3054e21c14cec7281aa8
romulovieira777 <romulo.vieira777@gmail.com>
2020-12-17 19:37:54 -0300

    Operadores Relacionais

---
+++ Seção 10 - Operadores Relacionais/operadores_relacionais.py
@@ -0,0 +1,27 @@
+# Declarando Duas Variáveis
+x = 7
+y = 5
+
+
+# Comparando se X é maior que Y
+print(x > y)
+
+
+# Comparando se X é maior ou igual Y
+print(x >= y)
+
+
+# Comparando se X é menor que Y
+print(x < y)
+
+
+# Comparando se X é menor ou igual Y
+print(x <= y)
+
+
+# Comprando se X é igual Y
+print(x == y)
+
+
+# Comparando se X é diferente de Y
+print(x != y)
