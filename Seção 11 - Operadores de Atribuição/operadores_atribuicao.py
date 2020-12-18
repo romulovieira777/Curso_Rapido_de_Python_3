7008388be030bfc43a14cfeee8c1cd07b692b871
romulovieira777 <romulo.vieira777@gmail.com>
2020-12-18 20:56:19 -0300

    Operadores de Atribuição

---
+++ Seção 11 - Operadores de Atribuição/operadores_atribuicao.py
@@ -0,0 +1,39 @@
+# Declarando uma Variável
+resultado = 2
+print(resultado)
+
+
+# Declarando uma Variável
+resultado = 3
+print(resultado)
+
+
+# Somando Duas Váriáveis
+resultado = 2
+resultado += 3
+print(resultado)
+
+
+# Subtraindo Valor das Variáveis
+resultado = 2
+resultado += 3
+resultado -= 1
+print(resultado)
+
+
+# Multiplicando Valor da Variável
+resultado = 3
+resultado *= 4
+print(resultado)
+
+
+# Dividindo Duas Variáveis
+resultado = 4
+resultado /= 2
+print(resultado)
+
+
+# Pegando o resto da Divisão
+resultado = 3
+resultado %= 2
+print(resultado)
