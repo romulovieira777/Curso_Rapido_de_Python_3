5b7ff4089a5b704a51807c77e380c7ae6cb4f2fb
romulovieira777 <romulo.vieira777@gmail.com>
2020-12-19 11:46:18 -0300

    Operadores Lógicos

---
+++ Seção 12 - Operadores Lógicos/operadores_logicos.py
@@ -0,0 +1,22 @@
+# Declarando Três Variáveis
+b1 = True
+b2 = False
+b3 = True
+
+
+# Comparando as Variáveis
+print(b1 and b2 and b3)
+
+
+# Chamando a Variável
+print(b1 or b2 or b3)
+
+
+# Comparando Duas Variáveis
+print(b1 != b2)
+
+
+# Negação da Variável
+print(not b1)
+print(not b2)
+print(b1 and not b2 and b3)
