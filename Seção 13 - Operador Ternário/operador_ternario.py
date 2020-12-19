5b85df28b1cceffdfc323662fb6baaa36c4a8220
romulovieira777 <romulo.vieira777@gmail.com>
2020-12-19 12:03:13 -0300

    Operador Ternário

---
+++ Seção 13 - Operador Ternário/operador_ternario.py
@@ -0,0 +1,18 @@
+# Delcarando Duas Variável
+lockdown = True
+grana = 30
+
+
+# Declarando uma Variável
+status = 'Em casa' if lockdown or grana <= 100 else 'Uhuuuu'
+
+
+# Imprimindo o Valor na Tela
+print(f'O status é: {status}')
+
+
+
+
+
+
+
